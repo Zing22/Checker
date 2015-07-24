eb99b39a99f33586aa9f1a88e886bb364ca9ea92
zing <zinglee@vip.qq.com>
2015-07-24 12:04:15 +0800

    单个查询的py2exe脚本

---
+++ singleTime/setup.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+
+from distutils.core import setup
+import py2exe
+
+setup(
+    windows=[
+        {"script":"main.py","icon_resources":[(1,"logo.ico"),]}],
+    options={
+        "py2exe":{"includes":["sip"],"dll_excludes":["MSVCP90.dll"],\
+            "bundle_files": 3,"optimize": 2,
+            }},
+    data_files=[
+        ("image", ["./logo.ico",])]
+    )
